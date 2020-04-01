b5cc7a98024f14c4ed778ff4eff205ffca867505
ngthhang <ngthhang9102000@gmail.com>
2020-04-01 20:22:43 +0700

    init project

---
+++ controller/MailController.php
@@ -0,0 +1,22 @@
+<?php
+    class MailControler{
+        public function index(){
+            
+        }
+
+        public function view()
+        {
+
+        }
+
+        public function reply()
+        {
+
+        }
+
+        public function forward()
+        {
+
+        }
+    }
+?>
